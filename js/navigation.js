8aec50ea0d0bf5b85367e226cfeca3bc9335b86b
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-08-19 00:42:54 +0200

    fixing mobile navigation, js code refractoring, map size changes

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,30 @@
-const burgerMenu = document.querySelector(".navigation__burger");
-const menu = document.querySelector(".navigation__right");
-const topLine = document.querySelector(".navigation__burger-line--top");
-const middleLine = document.querySelector(".navigation__burger-line--middle");
-const bottomLine = document.querySelector(".navigation__burger-line--bottom");
+{
+  const burgerMenu = document.querySelector(".navigation__burger");
+  const navLink = document.querySelectorAll(".navigation__link");
 
-burgerMenu.addEventListener("click", () => {
-  menu.classList.toggle("menu--open");
-  topLine.classList.toggle("topLine--open");
-  middleLine.classList.toggle("middleLine--open");
-  bottomLine.classList.toggle("bottomLine--open");
-  const isOpened = burgerMenu.getAttribute("aria-expanded") === "true";
+  const action = () => {
+    const menu = document.querySelector(".navigation__right");
+    const topLine = document.querySelector(".navigation__burger-line--top");
+    const middleLine = document.querySelector(
+      ".navigation__burger-line--middle",
+    );
+    const bottomLine = document.querySelector(
+      ".navigation__burger-line--bottom",
+    );
 
-  burgerMenu.classList.toggle("burger--open", !isOpened);
-  burgerMenu.setAttribute("aria-expanded", String(!isOpened));
-});
+    menu.classList.toggle("menu--open");
+    topLine.classList.toggle("topLine--open");
+    middleLine.classList.toggle("middleLine--open");
+    bottomLine.classList.toggle("bottomLine--open");
+    const isOpened = burgerMenu.getAttribute("aria-expanded") === "true";
+
+    burgerMenu.classList.toggle("burger--open", !isOpened);
+    burgerMenu.setAttribute("aria-expanded", String(!isOpened));
+  };
+
+  burgerMenu.addEventListener("click", action);
+
+  navLink.forEach((link) => {
+    link.addEventListener("click", action);
+  });
+}
