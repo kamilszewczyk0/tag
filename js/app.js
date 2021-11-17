6048ad7de970bb7c894074d0866e29792dee31d3
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2021-11-17 17:43:45 +0100

    removed photos, added new menu, added momondo button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 const masonry = new Macy({
   container: ".gallery__images",
   mobileFirst: false,
-  columns: 5,
+  columns: 4,
   breakAt: {
     450: 2,
     700: 3,
     1024: 4,
-    1200: 5,
+    // 1200: 5,
   },
   margin: {
     x: 30,
